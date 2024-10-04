6a6c62e9cb27a32f876e06eeb132f80624845018
Awatsathon Thoophom <awatsathon.t@outlook.com>
2024-10-04 16:37:11 +0700

    init go sum by import uuid

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 func main() {
-	fmt.Println("test")
+	id := uuid.New()
+
+	fmt.Print("test \n")
+	fmt.Printf("uuid: %s", id)
 }
