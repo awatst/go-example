e5977fb306e72c1732f65e61f456db2941469098
Awatsathon Thoophom <awatsathon.t@outlook.com>
2024-10-06 14:33:43 +0700

    add example map

--- main.go
+++ main.go
@@ -15,17 +15,50 @@ func main() {
 	a, b := service.SwapString("1", "2")
 	fmt.Printf("main_swapString: a: %s, b: %s", a, b)
 	exampleSlice()
+	exampleMap()
 }
 
 func exampleSlice() {
 	mySlice := []int{10, 20, 30, 40, 50}
-	fmt.Println("======================")
+
 	fmt.Println(mySlice)
 	fmt.Println(len(mySlice))
 	fmt.Println(cap(mySlice))
 
 	mySubSlice := mySlice[1:3] //select start at index 1 and end at index 3
+	fmt.Println("======================")
 	fmt.Println("subslice: ", mySubSlice)
 	fmt.Println("len subslice: ", len(mySubSlice))
-	fmt.Println("cap subslice: ", cap(mySubSlice))
+	fmt.Println("cap subslice: ", cap(mySubSlice)) //check memory, slice keep index 1 to end
+}
+
+func exampleMap() {
+	myMap := make(map[string]int)
+
+	myMap["Ford"] = 10
+	myMap["Honda"] = 20
+	myMap["Toyota"] = 30
+	myMap["Subaru"] = 40
+
+	delete(myMap, "Toyota")
+
+	fmt.Println("======================")
+	fmt.Println("Ford: ", myMap["Ford"])
+	fmt.Println("Toyota: ", myMap["Toyota"])
+
+	for key, value := range myMap {
+		// fmt.Printf("%s -> %d\n", key, value)
+		if key == "Subaru" {
+			fmt.Printf("%s -> %d\n", key, value)
+		}
+	}
+
+	//check if key is exists
+	val, exists := myMap["Toyota"]
+	if exists {
+		fmt.Println("Toyota: ", val)
+	} else {
+		fmt.Println("Toyota key not exists")
+	}
+
 }
